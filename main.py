4739d92e28929dc8bab47453be047de5ea2d2fa3
mike stedman <github@ravuya.com>
2014-10-31 18:42:01 -0600

    got some stuff from the flaskr demo

--- main.py
+++ main.py
@@ -24,5 +24,52 @@ def init_db():
 def connect_db():
   return sqlite3.connect(app.config['DATABASE'])
 
+# db bindings time
+@app.before_request
+def before_request():
+    g.db = connect_db()
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+@app.route('/')
+def show_parts():
+    cursor = g.db.execute('select id, name from parts order by id desc')
+    parts = [dict(title = row[0], text = row[1]) for row in cursor.fetchall()]
+    return render_template('show_parts.html', parts = parts)
+
+@app.route('/add', methods = ['POST'])
+def add_part():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into parts (name) values (?)', [request.form['name']])
+    g.db.commit()
+    flash('new part successfully posted')
+    return redirect(url_for('show_parts'))
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    error = None
+    if request.method == "POST":
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'invalid password.'
+        else:
+            session['logged_in'] = True
+            flash('you are logged in')
+            return redirect(url_for('show_parts'))
+    return render_template('login.html', error = error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None) # pop logged_in, replace with None
+    flash('logged out')
+    return redirect(url_for('show_parts'))
+
+
+# always at the bottom...
 if __name__ == '__main__':
   app.run()
